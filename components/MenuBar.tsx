29edd76ec1b2c56bb7d34997365f2f0c544c087e
MakayaYoel <yoelmakaya@gmail.com>
2025-02-12 12:47:14 -0500

    Feature: Implement Menu/Top Bar

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -1,31 +1,101 @@
-import React from 'react'
+"use client";
 
-const MenuBar = () => {
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Image from 'next/image';
+import { Button, Menu, MenuItem, styled, useMediaQuery, useTheme } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+import { MouseEvent, useEffect, useState } from 'react';
+import Link from 'next/link';
+import { SPACING, SIZES, COLORS } from '@/lib/stylingConstants';
+
+export default function ButtonAppBar() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const [anchorElNav, setAnchorElNav] = useState<HTMLElement | null>(null);
+
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget as HTMLElement);
+  };
+  
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      handleCloseNavMenu(); // Close menu when screen resizes
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const NavButton = styled(Button)({
+    color: 'white',
+    fontWeight: 700
+  });
+
   return (
-    <div className="w-full h-[63px] flex justify-between items-center bg-[#1D5AD5] px-20">
-      <div>
-        <img
-          src="/mentra_logo.png"
-          alt="Logo Image"
-          style={{ width: '68px' }}
-        />
-      </div>
-      <ul className="flex text-[15px] font-roboto font-[700] text-white w-1/4 justify-between">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">About Us</a>
-        </li>
-        <li>
-          <a href="/">Services</a>
-        </li>
-        <li>
-          <a href="/">Contact</a>
-        </li>
-      </ul>
-    </div>
-  )
-}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" sx={{ backgroundColor: COLORS.SECONDARY }}>
+        <Toolbar sx={{ justifyContent: "space-between", px: SPACING.SM_SPACING }} disableGutters>
+          <Image 
+            src='/mentra_logo.png'
+            width={SIZES.LOGO_IMG.WIDTH}
+            height={SIZES.LOGO_IMG.HEIGHT}
+            alt='Mentra Inc LOGO_IMG'
+          />
 
-export default MenuBar
+          {!isMobile ?
+            (
+              <Box sx={{ display: "flex", gap: SPACING.XS_SPACING }}>
+                <NavButton href="#home">Home</NavButton>
+
+                <NavButton href="#about-us">About Us</NavButton>
+                
+                <NavButton href="#services">Services</NavButton>
+
+                <NavButton href="/">Contact</NavButton>
+            </Box>
+            ) : 
+            (
+              <Box>
+                  <IconButton 
+                    size="large"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                  >
+                  <MenuIcon />
+                </IconButton>
+
+                <Menu
+                  anchorEl={anchorElNav}
+                  keepMounted
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                >
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Link href="#home">Home</Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Link href="#about-us">About Us</Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Link href="#services">Services</Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Link href="#contact">Contact</Link>
+                  </MenuItem>
+                </Menu>
+              </Box>
+            )
+          }
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ lib/stylingConstants.ts
@@ -0,0 +1,33 @@
+export const COLORS = {
+    PRIMARY: "#0D378B",
+    SECONDARY: "#1D5AD5",
+    BACKGROUND: "#98c4fc",
+    TEXT_OPACITY: 0.8,
+};
+  
+export const SPACING = {
+    TEXT_LINE_HEIGHT: 3,
+    XS_SPACING: 2,
+    SM_SPACING: 6,
+    MD_SPACING: 12,
+    LG_SPACING: 18,
+    XL_SPACING: 24,
+};
+  
+export const FONT_SIZES = {
+    HEADING_LG: "3em",
+    HEADING_SM: '2.5em',
+    BODY: "1.2em",
+    SUBHEADING: "1.5em",
+    PRODUCT_TEXT: "1.2em",
+};
+  
+export const SIZES = {
+    ABOUT_US_IMG: { WIDTH: 458, HEIGHT: 371 },
+    HERO_IMG: { WIDTH: 494, HEIGHT: 442 },
+    LOGO_IMG: { WIDTH: 68, HEIGHT: 50 },
+
+    PRODUCT_CARD: { MAX_WIDTH: 575 },
+    ABOUT_US_TEXT: { MAX_WIDTH: 630 },
+};
+  

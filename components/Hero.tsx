f2865538f58999ccd4a5fd7da1caab1c8866ce77
MakayaYoel <yoelmakaya@gmail.com>
2025-02-20 10:41:10 -0500

    replace useMediaQuery() with MUI breakpoints

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,68 +1,94 @@
 import Image from "next/image";
-import { Box, Typography, Stack, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Typography, Stack, useTheme } from "@mui/material";
 import WaveDivider from "./WaveDivider";
 import { SIZES, SPACING, FONT_SIZES, TEXT } from "@/lib/stylingConstants";
 import Heading from "./Heading";
 
 export default function Hero() {
     const theme = useTheme();
-    const isSmallLaptop = useMediaQuery(theme.breakpoints.down('lg'));
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
-        <Box sx={{ 
-            minHeight: '100vh', 
-            backgroundColor: theme.palette.background.default, 
-            position: 'relative', 
-            display: "flex", 
-            justifyContent: 'center', 
-            alignItems: 'center' 
-        }}>
-            <Box sx={{ 
-                display: 'flex', 
-                gap: isSmallLaptop ? SPACING.SM_SPACING : SPACING.MD_SPACING, 
-                flexDirection: isSmallLaptop ? 'column' : 'row', 
-                py: isSmallLaptop ? SPACING.MD_SPACING : 0,
-                px: isSmallLaptop ? SPACING.XS_SPACING : 0,
-                alignItems: isSmallLaptop ? 'center' : 'normal'
-            }}>
+        <Box
+            sx={{
+                minHeight: "100vh",
+                backgroundColor: theme.palette.background.default,
+                position: "relative",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+        >
+            <Box
+                sx={{
+                    display: "flex",
+                    gap: {
+                        xs: SPACING.SM_SPACING,
+                        lg: SPACING.MD_SPACING
+                    },
+                    flexDirection: {
+                        xs: "column",
+                        lg: "row"
+                    },
+                    py: {
+                        xs: SPACING.MD_SPACING,
+                        lg: 0
+                    },
+                    px: {
+                        xs: SPACING.XS_SPACING,
+                        lg: 0
+                    },
+                    alignItems: {
+                        xs: "center",
+                        lg: "normal"
+                    }
+                }}
+            >
                 <Stack>
                     <Image
-                        src='/hero_img.png'
+                        src="/hero_img.png"
                         width={SIZES.HERO_IMG.WIDTH}
                         height={SIZES.HERO_IMG.HEIGHT}
                         alt="Hero Image"
                     />
                 </Stack>
 
-                <Stack 
-                    spacing={SPACING.MD_SPACING} 
-                    textAlign={isSmallLaptop ? 'center' : 'left'}
+                <Stack
+                    spacing={SPACING.MD_SPACING}
+                    textAlign={{
+                        xs: "center",
+                        lg: "left"
+                    }}
                 >
                     <Stack spacing={SPACING.XS_SPACING}>
-                        <Heading title='Empowering Graduates.' />
-                        <Heading title='Supporting Businesses.' />
-                        <Heading title='Transforming Careers.' />
+                        <Heading title="Empowering Graduates." />
+                        <Heading title="Supporting Businesses." />
+                        <Heading title="Transforming Careers." />
                     </Stack>
 
-                    <Typography 
-                        variant="h2" 
-                        sx={{ 
-                            fontSize: FONT_SIZES.BODY, 
-                            fontWeight: 600, 
-                            color: theme.palette.primary.main, 
-                            my: SPACING.SM_SPACING, 
-                            maxWidth: SIZES.ABOUT_US_TEXT.MAX_WIDTH ,
+                    <Typography
+                        variant="h2"
+                        sx={{
+                            fontSize: FONT_SIZES.BODY,
+                            fontWeight: 600,
+                            color: theme.palette.primary.main,
+                            my: SPACING.SM_SPACING,
+                            maxWidth: SIZES.ABOUT_US_TEXT.MAX_WIDTH,
                             opacity: TEXT.TEXT_OPACITY
                         }}
                     >
                         From Learning to Earning: Mentra bridges the gap between academic knowledge and real-world success.
                     </Typography>
 
-                    <Stack 
-                        direction={isMobile ? 'column' : 'row'} 
-                        justifyContent={isSmallLaptop ? 'center' : 'space-between'} 
-                        alignItems='center' 
+                    <Stack
+                        direction={{
+                            xs: "column",
+                            sm: "row"
+                        }}
+                        justifyContent={{
+                            xs: "center",
+                            lg: "space-between"
+                        }}
+                        alignItems="center"
                         spacing={SPACING.XS_SPACING}
                     >
                         <button className="btn-custom">Start Your Journey</button>

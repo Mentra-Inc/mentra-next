0a1170671ac0339bc2e9df3a9bd4a77df004376c
MakayaYoel <yoelmakaya@gmail.com>
2025-02-24 09:12:48 -0500

    style at element level

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import Hero from '@/components/Hero';
-import MenuBar from '@/components/MenuBar'
 import theme from '@/theme/theme';
 import { ThemeProvider } from '@mui/material';
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
-      <main>
-        {/* <MenuBar /> */}
+      <main style={{ overflow: 'hidden' }}>
         <Hero />
       </main>
     </ThemeProvider>

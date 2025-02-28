8bfb8cb618bddaf6f3a2cf62abb15f449001ba48
MakayaYoel <yoelmakaya@gmail.com>
2025-02-28 10:48:17 -0500

    add productData in OurProducts component

--- components/OurProducts.tsx
+++ components/OurProducts.tsx
@@ -1,8 +1,26 @@
 import { Container, Stack } from "@mui/material";
 import Heading from "./Heading";
-import productData from "@/lib/productData";
 import ProductCard from "./ProductCard";
 import CustomButton from "./CustomButton";
+
+const productData = [
+    {
+        id: 'mentorship',
+        cardTitle: "Mentorship & Real-World Training Program",
+        listItems: [
+            { id: "mentorship-1", text: "Hands-on experience working on real projects commissioned by investor clients." },
+            { id: "mentorship-2", text: "Personalized one-on-one mentorship to prepare graduates for industry demands." },
+        ],
+    },
+    {
+        id: 'affordable-mvp',
+        cardTitle: "Affordable MVP Development for Businesses",
+        listItems: [
+            { id: "affordable-1", text: "Cost-effective or free website development tailored to your startup's needs." },
+            { id: "affordable-2", text: "Collaborate with skilled graduates eager to bring your ideas to life." },
+        ],
+    },
+];
 
 export type ProductListItem = { 
     id: string, 
